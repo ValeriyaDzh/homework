cc568cb6608621840b8596230d6ad2bdf030d365
Valeriya <Dzhavadova.v@yandex.ru>
2024-08-27 14:57:38 +0300

    add database

---
+++ databases/bookshop/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import sessionmaker
+
+from .config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
+
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(engine)
+
+
+# Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
+
+
+def create_db():
+    Base.metadata.create_all(engine)
