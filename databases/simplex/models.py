ed58593a90184616bbf189faf97071dd8f2a21a5
Valeriya <Dzhavadova.v@yandex.ru>
2024-09-13 11:09:04 +0300

    add CheckConstraint for positive int

--- databases/simplex/models.py
+++ databases/simplex/models.py
@@ -1,6 +1,6 @@
 import uuid
 from datetime import datetime, date
-from sqlalchemy import UUID
+from sqlalchemy import UUID, CheckConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database import Base
@@ -19,11 +19,17 @@ class SpimexTradingResults(Base):
     delivery_basis_id: Mapped[str]
     delivery_basis_name: Mapped[str]
     delivery_type_id: Mapped[str]
-    volume: Mapped[int] = mapped_column(nullable=False)
-    total: Mapped[int] = mapped_column(nullable=False)
-    count: Mapped[int] = mapped_column(nullable=False)
+    volume: Mapped[int]
+    total: Mapped[int]
+    count: Mapped[int]
     date: Mapped[date]
     created_on: Mapped[datetime] = mapped_column(default=datetime.now)
     updated_on: Mapped[datetime] = mapped_column(
         default=datetime.now, onupdate=datetime.now
+    )
+
+    __table_args__ = (
+        CheckConstraint("volume >= 0", name="check_volume_positive"),
+        CheckConstraint("total >= 0", name="check_total_positive"),
+        CheckConstraint("count >= 0", name="check_count_positive"),
     )
